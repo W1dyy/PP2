d460fce974ac43df1722dde70dd2dd0cd8c02be6
W1dyy <168451876+W1dyy@users.noreply.github.com>
2025-01-26 17:59:40 +0500

    Add files via upload

---
+++ Lab2/Task2.py
@@ -0,0 +1,85 @@
+print(10+5)
+
+print(10-5)
+
+print(10*5)
+
+print(10/5)
+
+print(10%5)
+
+print(10**5)
+
+print(10//5)
+
+x = 5
+
+x+=3
+
+print(x)
+
+x-=3
+
+print(x)
+
+x*=3
+
+print(x)
+
+x/=3
+
+print(x)
+
+x%=3
+
+print(x)
+
+x//=3
+
+print(x)
+
+x**=3
+
+print(x)
+
+x = 3
+
+print(x:=3)
+
+x = 0
+
+y = 1
+
+print(x==y)
+
+print(x!=y)
+
+print(x>y)
+
+print(x<y)
+
+print(x>=y)
+
+print(x<=y)
+
+print(x < 5 and  x < 10)
+
+print(x < 5 or x < 4)
+
+print(not(x < 5 and x < 10))
+
+print(x is y)
+
+print(x is not y)
+
+print(x&y)
+
+print(x | y)
+
+print(x ^ y)
+
+print(~x)
+
+print(x << 2)
+
+print(x >> 2)
